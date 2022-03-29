bfd8e8e25844f80db504aa47d9772dd2f80611f5
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2022-03-29 18:15:14 +0300

    Trying some things with sound and notifications

--- src/composables/echo-plugin.ts
+++ src/composables/echo-plugin.ts
@@ -3,6 +3,8 @@ import { registerPlugin } from '@capacitor/core'
 export interface EchoPlugin {
   echo(options: { value: string }): Promise<{ value: string }>
   print(options: { text: string }): Promise<void>
+  ring(): Promise<void>
+  notify(options: { title: string; content: string }): Promise<{ status: boolean }>
 }
 
 const Echo = registerPlugin<EchoPlugin>('Echo')
--- src/views/AlarmView.vue
+++ src/views/AlarmView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-// import { ref } from 'vue'
+import { ref } from 'vue'
 import type { PluginResultError } from '@capacitor/core'
 import Echo from '~/composables/echo-plugin'
 import Header from '~/components/Header.vue'
@@ -7,7 +7,7 @@ import Footer from '~/components/TabBar.vue'
 // import SortBar from '~/components/SortBar.vue'
 // import Allarms from '~/components/Allarms.vue'
 
-// const val = ref('not changes')
+const val = ref(false)
 
 // const { value } = await Echo.echo({ value: 'Changes!' })
 //   .catch((error: PluginResultError) => {
@@ -17,8 +17,31 @@ import Footer from '~/components/TabBar.vue'
 // console.error(value)
 // val.value = value
 
+// just checking things out
 const showToast = async(value: string) => {
   await Echo.print({ text: value })
+    .catch((error: PluginResultError) => {
+      console.error(error.message)
+    })
+}
+
+const ringAlarm = async() => {
+  await Echo
+    .ring()
+    .then(() => {
+      showToast('Ring!')
+    })
+    .catch((error: PluginResultError) => {
+      console.error(error.message)
+    })
+}
+
+const notifyNotification = async(title: string, content: string) => {
+  await Echo
+    .notify({ title, content })
+    .then((returnedValue) => {
+      val.value = returnedValue.status
+    })
     .catch((error: PluginResultError) => {
       console.error(error.message)
     })
@@ -29,11 +52,21 @@ const showToast = async(value: string) => {
 <template>
   <Header />
 
-  <main>
-    <h1 @click="showToast('hello')">
-      Click here!
-    </h1>
-    <div class="w-full h-screen bg-red-100" />
+  <main class="flex flex-col p-1em gap-y-1em">
+    <div class="rounded-1.5em text-center bg-green-25 p-1em" @click="showToast('hello')">
+      <p>Click here! </p>
+    </div>
+
+    <div class="rounded-1.5em text-center bg-blue-25 p-1em" @click="ringAlarm()">
+      <p>Ring ring!</p>
+    </div>
+
+    <div class="rounded-1.5em text-center bg-yellow-25 p-1em" @click="notifyNotification('SRUN Wakeup!', 'Alarming bruh...')">
+      <p>Notify!</p>
+      <p>{{ val }}</p>
+    </div>
+
+    <div class="w-full h-screen bg-red-25 rounded-2.265em" />
   </main>
 
   <Footer />
